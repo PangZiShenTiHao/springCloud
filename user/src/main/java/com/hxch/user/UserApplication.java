e422a8460531c728e05d98c93d226f78d41fefa0
huangxc <huangxianc@jkinvest.com>
2019-12-11 11:48:57 +0800

    user微服务初始化

--- user/src/main/java/com/hxch/user/UserApplication.java
+++ user/src/main/java/com/hxch/user/UserApplication.java
@@ -2,12 +2,16 @@ package com.hxch.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+        System.out.println("user ... user启动成功 ... ");
     }
+
 
 }
