3b231a9f8f1f1372703e38335ab754bf336be921
huangxc <huangxianc@jkinvest.com>
2019-12-12 15:59:35 +0800

    zuul配置 并成功访问user微服务

--- user/src/main/java/com/hxch/user/UserApplication.java
+++ user/src/main/java/com/hxch/user/UserApplication.java
@@ -1,17 +1,19 @@
 package com.hxch.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @EnableEurekaClient
 @SpringBootApplication
 public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
-        System.out.println("user ... user启动成功 ... ");
+        log.info("user ... user启动成功 ... ");
     }
 
 
--- zuul-gateway/src/main/java/com/hxch/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/hxch/zuulgateway/ZuulGatewayApplication.java
@@ -1,13 +1,20 @@
 package com.hxch.zuulgateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@Slf4j
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
+        log.info("zuul-gateway ... 启动成功");
     }
 
 }
